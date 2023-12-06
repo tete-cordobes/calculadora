c7f7273cfe37a0ffe0950aff3ae4d95ba127069a
tete-cordobes <45921417+tete-cordobes@users.noreply.github.com>
2023-12-06 21:08:31 +0100

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,22 @@
-
 import streamlit as st
 
+# URL del logo
+logo_url = "https://yinyangseo.com/wp-content/uploads/2022/06/Recurso-26.png"
+
+# Mostrar el logo en la parte superior de la aplicación
+st.image(logo_url, width=150)
+
 # Título de la aplicación
-st.title('Calculadora Básica')
+st.title('Calculadora YingYangSeo')
 
-# Entradas para los números
-num1 = st.number_input('Introduce el primer número', format='%f')
-num2 = st.number_input('Introduce el segundo número', format='%f')
+# Sidebar para las entradas de números
+with st.sidebar:
+    st.header("Entradas")
+    num1 = st.selectbox('Introduce el primer número', range(1, 10))
+    num2 = st.selectbox('Introduce el segundo número', range(1, 10))
 
-# Opciones para operaciones
-operation = st.selectbox('Elige una operación', ['Sumar', 'Restar', 'Multiplicar', 'Dividir'])
+    # Opciones para operaciones
+    operation = st.selectbox('Elige una operación', ['Sumar', 'Restar', 'Multiplicar', 'Dividir'])
 
 # Función para realizar las operaciones
 def calculate(num1, num2, operation):
@@ -28,4 +35,5 @@ def calculate(num1, num2, operation):
 # Mostrar el resultado
 if st.button('Calcular'):
     result = calculate(num1, num2, operation)
-    st.success(f'El resultado es: {result}')
+    st.header('Resultado:')
+    st.success(f'{result}')
